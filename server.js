0f00235330a96a5a67ac68f3f3d1115134f5f04f
zophrox <zophrox@gmail.com>
2021-07-30 14:38:22 +0300

    test

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/generate-string'));
+app.use(express.static(__dirname + '/dist/generate-string'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/generate-string/'}),
-);
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/generate-string/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
